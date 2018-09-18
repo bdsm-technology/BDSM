eaac733bcc8cbd329df5d091d838b517e5490d4b
CodeHz <codehz@outlook.com>
2018-09-18 13:28:28 +0800

    Add attach command
    Signed-off-by: CodeHz <codehz@outlook.com>

---
+++ attach.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/user"
+	"strings"
+
+	"github.com/chzyer/readline"
+	"github.com/valyala/fasttemplate"
+)
+
+func attach(profile string, usermode, keep bool, prompt *fasttemplate.Template) {
+	var bus bus
+	bus.init(profile)
+	defer bus.close()
+	vs, err := bus.ping()
+	if err != nil {
+		printWarn("Server is not running!")
+		if !keep {
+			return
+		}
+	} else {
+		printPair("Server Version: %s", vs)
+	}
+
+	username := "nobody"
+	hostname := "bedrockserver"
+	{
+		u, err := user.Current()
+		if err == nil {
+			username = u.Username
+		}
+		hn, err := os.Hostname()
+		if err == nil {
+			hostname = hn
+		}
+	}
+
+	rl, _ := readline.NewEx(&readline.Config{
+		Prompt: prompt.ExecuteString(map[string]interface{}{
+			"username": username,
+			"hostname": hostname,
+			"esc":      "\033",
+		}),
+		HistoryFile:     ".readline-history",
+		AutoComplete:    bus,
+		InterruptPrompt: "^C",
+		EOFPrompt:       ":quit",
+
+		HistorySearchFold: true,
+		FuncFilterInputRune: func(r rune) (rune, bool) {
+			if r == readline.CharCtrlZ {
+				return r, false
+			}
+			return r, true
+		},
+	})
+	defer rl.Close()
+	lw := rl.Stdout()
+	execFn := func(src, cmd string) {
+		ncmd := strings.TrimSpace(cmd)
+		if len(ncmd) == 0 {
+			return
+		}
+		result, err := bus.exec(ncmd)
+		if err != nil {
+			fmt.Fprintf(lw, "\033[0m%v\033[0m\n", err)
+		} else {
+			if len(result) > 0 {
+				fmt.Fprintf(lw, "\033[0m%s\033[0m", replacer.Replace(result))
+			}
+		}
+	}
+	go func() {
+		for v := range bus.log {
+			v.write(lw)
+		}
+	}()
+	for {
+		line, err := rl.Readline()
+		if err == readline.ErrInterrupt {
+			if len(line) == 0 {
+				break
+			} else {
+				continue
+			}
+		} else if err == io.EOF {
+			break
+		}
+		line = strings.TrimSpace(line)
+		execFn("console", line)
+	}
+	bus.stop()
+}
--- main.go
+++ main.go
@@ -48,6 +48,37 @@ func main() {
 				return nil
 			},
 		},
+		{
+			Name:    "attach",
+			Aliases: []string{"a"},
+			Usage:   "Attach bedrock server console",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "profile, p",
+					Value:  "default",
+					Usage:  "Profile `name`",
+					EnvVar: "profile",
+				},
+				cli.BoolFlag{
+					Name:   "user, u",
+					Usage:  "Using user dbus",
+					EnvVar: "user_dbus",
+				},
+				cli.StringFlag{
+					Name:  "prompt",
+					Value: "{{esc}}[0;36;1mbedrock_server:{{esc}}[22m//{{username}}@{{hostname}}$ {{esc}}[33;0m",
+					Usage: "Prompt `template`",
+				},
+				cli.BoolFlag{
+					Name:  "force, f",
+					Usage: "Keep connection even server is not running",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				attach(c.String("profile"), c.Bool("user"), c.Bool("force"), fasttemplate.New(c.String("prompt"), "{{", "}}"))
+				return nil
+			},
+		},
 	}
 
 	err := app.Run(os.Args)
